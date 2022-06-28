2852caf626551bee59d1838c89518ac855d2c24f
ge86soz <ge68soz@um.de>
2022-06-28 16:17:21 +0200

    First ethereum contract

---
+++ contracts/Greeting.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.8.0;
+
+contract Greeter {
+    string private greeting;
+
+    constructor(string memory _greeting) {
+        greeting = _greeting;
+    }
+    function greet() public view returns (string memory){
+        return greeting;
+    }
+}
